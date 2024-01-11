b95e2cbf6b750fd66c57dfc2d8079bd8a9bf2f36
Saul Paredes <saulkurosaki13@gmail.com>
2024-01-11 15:41:41 -0600

    setup hardhat.config.js file

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.0",
+  networks: {
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/EnpYPXfAO3tjRLKV9tlsBxQ9iZwxauoX",
+      accounts: [
+        "82e8ddca2489862341a1f2e09708c67030987763e44530cda5ebb6f0d01741a3",
+      ],
+    },
+  },
 };
