79dbec00368893c5213c5d2c3c4c505eeb7dc842
Saul Paredes <saulkurosaki13@gmail.com>
2024-01-11 12:30:44 -0600

    feat(Transactions.sol): implement solidity transactions contract initial layout

---
+++ smart_contract/contracts/Transactions.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+contract Transactions {
+  uint256 transactionCounter;
+
+  event Transfer(address from, address receiver, uint amount, string message, uint265 timestamp, string keyword);
+
+  struct TransferStruct {
+    address sender;
+    address receiver;
+    uint amount;
+    string message;
+    uint265 timestamp;
+    string keyword;
+  }
+
+  TransferStruct[] transactions;
+
+  function addToBlockchain() public {
+
+  }
+
+    function getAllTransactions() public view returns (TransferStruct[] memory) {
+    // return transactions
+  }
+
+    function getTransactionCount() public view returns (uint256) {
+    // return transaction count
+  }
+}
