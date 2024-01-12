b48e5c8b6afaeb9b6f0d0870c4027de04eed5745
Saul Paredes <saulkurosaki13@gmail.com>
2024-01-12 17:55:58 -0600

    feat(TransactionContext): implement createEthereumContract function

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -7,17 +7,17 @@ export const TransactionContext = React.createContext();
 
 const { ethereum } = window;
 
-// const createEthereumContract = () => {
-//   const provider = new ethers.providers.Web3Provider(ethereum);
-//   const signer = provider.getSigner();
-//   const transactionsContract = new ethers.Contract(
-//     contractAddress,
-//     contractABI,
-//     signer
-//   );
+const createEthereumContract = () => {
+  const provider = new ethers.providers.Web3Provider(ethereum);
+  const signer = provider.getSigner();
+  const transactionsContract = new ethers.Contract(
+    contractAddress,
+    contractABI,
+    signer
+  );
 
-//   return transactionsContract;
-// };
+  return transactionsContract;
+};
 
 export const TransactionProvider = ({ children }) => {
   const [formData, setFormData] = useState({
