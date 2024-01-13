16e426e936fa6dab66935ec6e2c3299eedc99e9b
Saul Paredes <saulkurosaki13@gmail.com>
2024-01-13 13:07:57 -0600

    fix(Services): fix layout bug

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -3,14 +3,14 @@ import { BiSearchAlt } from "react-icons/bi";
 import { RiHeart2Fill } from "react-icons/ri";
 
 const ServiceCard = ({ color, title, icon, subtitle }) => (
-  <div className="flex flex-row justify-start items-center white-glassmorphism p-3 m-2 cursor-pointer hover:shadow-xl">
+  <div className="flex max-lg:w-72 flex-row justify-start items-center white-glassmorphism p-3 m-2 cursor-pointer hover:shadow-xl">
     <div
       className={`w-10 h-10 rounded-full flex justify-center items-center ${color}`}
     >
       {icon}
     </div>
 
-    <div className="ml-5 flex flex-col flex-1">
+    <div className="ml-5 flex flex-col flex-1 mb-3">
       <h1 className="mt-2 text-white text-lg">{title}</h1>
       <p className="mt-2 text-white text-sm md:w-9/12">{subtitle}</p>
     </div>
@@ -19,8 +19,8 @@ const ServiceCard = ({ color, title, icon, subtitle }) => (
 
 const Services = () => {
   return (
-    <div className="flex flex-col md:flex-row w-full justify-center items-center gradient-bg-services">
-      <div className="flex mf:flex-row flex-col items-center justify-between md:p-20 py-12 px-4">
+    <div className="flex flex-col md:flex-row md:pr-16 w-full justify-center items-center gradient-bg-services lg:pr-14 2xl:px-20 2xl:pl-80">
+      <div className="flex mf:flex-row flex-col items-center justify-between md:p-20 md:pr-16 py-12 px-4">
         <div className="flex-1 flex flex-col justify-start items-start">
           <h1 className="text-white text-3xl sm:text-5xl py-2 text-gradient">
             Services that we
